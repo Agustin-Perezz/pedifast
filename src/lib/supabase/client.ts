65d344aa5836d089d7356cf1a89ebaa299c73a89
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-02-16 09:12:42 -0300

    Remove Supabase auth files and routes

--- src/lib/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '$env/dynamic/public';
-import { createBrowserClient } from '@supabase/ssr';
-
-export function createClient() {
-  return createBrowserClient(
-    env.PUBLIC_SUPABASE_URL!,
-    env.PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
