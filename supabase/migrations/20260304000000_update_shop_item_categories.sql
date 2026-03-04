6fb16a5be38fbbf44d38bb02af312597a3d14859
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-03-04 09:53:54 -0300

    Add milanesas and bebidas to shop_item_category enum in migration

--- supabase/migrations/20260304000000_update_shop_item_categories.sql
+++ supabase/migrations/20260304000000_update_shop_item_categories.sql
@@ -5,7 +5,7 @@ DROP TYPE public.shop_item_category;
 UPDATE public.shop_items SET category = 'hamburguesas';
 
 CREATE TYPE public.shop_item_category AS ENUM (
-  'pizzas', 'hamburguesas', 'empanadas', 'sandwiches', 'ensaladas', 'papas'
+  'pizzas', 'hamburguesas', 'empanadas', 'sandwiches', 'ensaladas', 'papas', 'milanesas', 'bebidas'
 );
 
 ALTER TABLE public.shop_items
