6f3bf7ece204d1c2fb627ce78f17f621d82fe554
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-03-20 17:34:45 -0300

    Update CSP to allow Google Fonts, Supabase images, and MercadoPago workers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -24,12 +24,13 @@ const securityHeaders: Handle = async ({ event, resolve }) => {
       'Content-Security-Policy',
       [
         "default-src 'self'",
-        "script-src 'self' 'unsafe-inline' https://sdk.mercadopago.com https://http2.mlstatic.com",
-        "style-src 'self' 'unsafe-inline'",
-        "img-src 'self' data: https://http2.mlstatic.com",
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sdk.mercadopago.com https://http2.mlstatic.com blob:",
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+        "img-src 'self' data: https://http2.mlstatic.com https://*.supabase.co",
         'frame-src https://*.mercadopago.com.ar https://*.mercadopago.com',
         "connect-src 'self' https://api.mercadopago.com https://*.sentry.io",
-        "font-src 'self'"
+        "font-src 'self' https://fonts.gstatic.com",
+        "worker-src 'self' blob:"
       ].join('; ')
     );
   }
